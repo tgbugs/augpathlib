ee3801b6f40b5af0c4ba48622a3c617d3b3c5fdf
Tom Gillespie <tgbugs@gmail.com>
2019-09-19 20:16:12 -0700

    setup.py add missing dependency

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     python_requires='>=3.6',
     tests_require=tests_require,
     install_requires=[
+        'dateutil',
         'python-magic',  # FIXME conflicts with sys-apps/file python bindings
         'pyxattr',
         'terminaltables',
